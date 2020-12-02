8ec13f0b9ce1def68215f513056bfc4b9387a5a5
Zhaslan <zhas1an10@yandex.ru>
2020-12-03 03:06:30 +0600

    create header, first and second sections

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".photo-pagination",
+    bulletClass: "photo-bullet",
+    bulletActiveClass: "photo-bullet-active",
+    clickable: true,
+    loop: true,
+  },
+});
