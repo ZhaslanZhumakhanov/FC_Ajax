078c15f43119f3b8f90c447023d081fc66f158ad
Zhaslan <zhas1an10@yandex.ru>
2020-12-04 00:06:08 +0600

    create main

--- js/main.js
+++ js/main.js
@@ -9,6 +9,12 @@ var mySwiper1 = new Swiper(".swiper-container1", {
 });
 
 var mySwiper2 = new Swiper(".swiper-container2", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".games-pagination",
     bulletClass: "games-bullet",
