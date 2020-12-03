c83ed1eaf55447c1e0839b174fb4f201150899ac
Zhaslan <zhas1an10@yandex.ru>
2020-12-03 21:06:15 +0600

    create third section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-var mySwiper = new Swiper(".swiper-container", {
+var mySwiper1 = new Swiper(".swiper-container1", {
   pagination: {
     el: ".photo-pagination",
     bulletClass: "photo-bullet",
     bulletActiveClass: "photo-bullet-active",
+    clickable: true,
+    loop: true,
+  },
+});
+
+var mySwiper2 = new Swiper(".swiper-container2", {
+  pagination: {
+    el: ".games-pagination",
+    bulletClass: "games-bullet",
+    bulletActiveClass: "games-bullet-active",
     clickable: true,
     loop: true,
   },
