8cd22975fdab6091ff97c34e7973d853b992816e
Zhaslan <zhas1an10@yandex.ru>
2020-12-04 22:47:52 +0600

    fix main

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 var mySwiper1 = new Swiper(".swiper-container1", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".photo-pagination",
     bulletClass: "photo-bullet",
